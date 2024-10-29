86cdc9288261196a5141d425819583a0e908eaf2
VictorLeon04 <134532043+VictorLeon04@users.noreply.github.com>
2024-10-29 12:10:21 +0000

    cambios

---
+++ dist/app.module.js
@@ -0,0 +1,27 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
+const mongoose_1 = require("@nestjs/mongoose");
+let AppModule = class AppModule {
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [],
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
+    }),
+    (0, common_1.Module)({
+        imports: [mongoose_1.MongooseModule.forRoot('mongodb+srv://vleonpatron06:3H6ggbbKS9D5lzjh@cluster0.amgf5.mongodb.net/db_translator?retryWrites=true&w=majority&appName=Cluster0')],
+    })
+], AppModule);
+//# sourceMappingURL=app.module.js.map
---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb+srv://vleonpatron06:3H6ggbbKS9D5lzjh@cluster0.amgf5.mongodb.net/db_translator?retryWrites=true&w=majority&appName=Cluster0')],
+})
+export class AppModule {}
