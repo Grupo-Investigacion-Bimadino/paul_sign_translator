568137fdf01cd10830e10fa1a9fad11a3bbc6813
Jorge18xd <136493359+Jorge18xd@users.noreply.github.com>
2024-11-01 21:02:31 +0000

    cambios

--- src/archivos/archivos.module.ts
+++ src/archivos/archivos.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ArchivosService } from './archivos.service';
 import { ArchivosController } from './archivos.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Archivos, ArchivosSchema } from './schemas/Archivos.schema';
+
 
 @Module({
-  controllers: [ArchivosController],
-  providers: [ArchivosService],
-})
+  imports: [
+    // adicionar esto
+    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+    //hasta aqui
+  ],
+
 export class ArchivosModule {}
